2e89f2d6b158700d4993b2faca2f0b3e73700123
himansu naik <naikhimansu546@gmail.com>
2025-01-19 17:03:35 +0530

    updated

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const menuBtn = document.querySelector(".menus");
 const menusItems = document.querySelector("#menu-items");
 const closeMenu = document.querySelector("#close-menu");
+const menus = document.querySelector(".menus");
 
 
 menuBtn.addEventListener("click", () => {
    menusItems.style.display = "block";
+   menus.style.display = "none"
 })
 
 closeMenu.addEventListener("click", () => {
    menusItems.style.display = "none";
+   menus.style.display = "block"
 })
