f5e54e25a03eb6782711edbd85613a8653265e5c
himansu naik <naikhimansu546@gmail.com>
2025-02-22 17:43:57 +0530

    add blur effect

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const menusItems = document.querySelector("#menu-items");
 const closeMenu = document.querySelector("#close-menu");
 const menus = document.querySelector(".menus");
 
+let blurLimit = 30;
+document.body.style.filter = `blur(${blurLimit}px)`
 
 menuBtn.addEventListener("click", () => {
    menusItems.style.display = "block";
@@ -13,3 +15,11 @@ closeMenu.addEventListener("click", () => {
    menusItems.style.display = "none";
    menus.style.display = "block"
 })
+
+ setInterval(() => {
+            if (blurLimit > 0) {
+               blurLimit--;
+               document.body.style.filter = `blur(${blurLimit}px)`
+            }
+          }, 80)
+
